7ae1ee4d0b1b376903f6082081f2a32206a66d07
hbilgi <hakanbilgi09@hotmail.com>
2015-02-17 23:51:35 -0600

    class exercise in week 2

---
+++ class2 exercise.py
@@ -0,0 +1,232 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:15:05) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 12
+12
+>>> hakan
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    hakan
+NameError: name 'hakan' is not defined
+>>> my_variable = 9
+>>> my_variable
+9
+>>> my_variable
+9
+>>> word = "NAU"
+>>> word
+'NAU'
+>>> print(10)
+10
+>>> print(my_variable)
+9
+>>> print(word)
+NAU
+>>> # We cannot use signs
+>>> name_school = NAU
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    name_school = NAU
+NameError: name 'NAU' is not defined
+>>> name_school = Hardward
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    name_school = Hardward
+NameError: name 'Hardward' is not defined
+>>> number = NAA
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    number = NAA
+NameError: name 'NAA' is not defined
+>>> number = 5
+>>> name_school = "nn"
+>>> name_school
+'nn'
+>>> My_name_is = "HAKAN"
+>>> my_name_is
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    my_name_is
+NameError: name 'my_name_is' is not defined
+>>> hakan == 261:
+	
+SyntaxError: invalid syntax
+>>> hakan == 261:
+	
+SyntaxError: invalid syntax
+>>> hakan = 261
+>>> if hakan == 261:
+	# do something
+	pass
+
+>>> if hakan == 261:
+	print(hakan)
+
+	
+261
+>>> if hakan ==259:
+	pass
+
+>>> print(hakan)
+261
+>>> 4*3
+12
+>>> 22-1
+21
+>>> 22/7
+3.142857142857143
+>>> ''' hbhjbjhbjnbnkjnkn bmjn nn'''
+' hbhjbjhbjnbnkjnkn bmjn nn'
+>>> #it is a line comment
+>>> '''dsfdsgdsfssdf'''
+'dsfdsgdsfssdf'
+>>> '''sdgdfgffgdf
+ddsf
+fdssd
+
+dfs
+df
+
+ds..'''
+'sdgdfgffgdf\nddsf\nfdssd\n\ndfs\ndf\n\nds..'
+>>> 3**9
+19683
+>>> 2**1
+2
+>>> 2***4
+SyntaxError: invalid syntax
+>>> 21/7
+3.0
+>>> 12.7/7.9
+1.6075949367088607
+>>> 8//8
+1
+>>> 8//4
+2
+>>> 5++6
+11
+>>> 5+++++6
+11
+>>> 16546546544654221546465845641321/888564513.2
+1.8621660328370427e+22
+>>> 1/2
+0.5
+>>> 4*3
+12
+>>> 9//5
+1
+>>> 3#4
+3
+>>> int area
+SyntaxError: invalid syntax
+>>> 9989899888888888888888888888888888888888888888888888989999999999999999999
+9989899888888888888888888888888888888888888888888888989999999999999999999
+>>> a = 3
+>>> b = 4
+>>> a+b
+7
+>>> a*b
+12
+>>> a**2
+9
+>>> 11-4*3/2
+5.0
+>>> 45.0//8.0
+5.0
+>>> 4i
+SyntaxError: invalid syntax
+>>> 9j
+9j
+>>> 5 pow 2
+SyntaxError: invalid syntax
+>>> type (10)
+<class 'int'>
+>>> type(.8)
+<class 'float'>
+>>> type(abc)
+Traceback (most recent call last):
+  File "<pyshell#83>", line 1, in <module>
+    type(abc)
+NameError: name 'abc' is not defined
+>>> type(6565656565656565656565656565666667j)
+<class 'complex'>
+>>> import math
+>>> type(math.pi)
+<class 'float'>
+>>> math.pi
+3.141592653589793
+>>> math.e
+2.718281828459045
+>>> math.65
+SyntaxError: invalid syntax
+>>> pow(3,2)
+9
+>>> 3**2
+9
+>>> abs(-10)
+10
+>>> round(2,8434)
+2
+>>> rounf(3,7)
+Traceback (most recent call last):
+  File "<pyshell#94>", line 1, in <module>
+    rounf(3,7)
+NameError: name 'rounf' is not defined
+>>> round(3,7)
+3
+>>> round(4.99768)
+5
+>>> int(3,445)
+Traceback (most recent call last):
+  File "<pyshell#97>", line 1, in <module>
+    int(3,445)
+ValueError: int() base must be >= 2 and <= 36
+>>> math.sin(90)
+0.8939966636005579
+>>> math.tan(45)
+1.6197751905438615
+>>> s1*3
+Traceback (most recent call last):
+  File "<pyshell#100>", line 1, in <module>
+    s1*3
+NameError: name 's1' is not defined
+>>> math.sin(math.radians(90))
+1.0
+>>> math.radians(90)
+1.5707963267948966
+>>> math.sin(pi)
+Traceback (most recent call last):
+  File "<pyshell#103>", line 1, in <module>
+    math.sin(pi)
+NameError: name 'pi' is not defined
+>>> 'hakan'
+'hakan'
+>>> ''hakan''
+SyntaxError: invalid syntax
+>>> "hakan"
+'hakan'
+>>> "knklk""kmks"
+'knklkkmks'
+>>> s= "kjkjl"
+>>> len(s)
+5
+>>> "hakan" + "bilgi"
+'hakanbilgi'
+>>> "hakan" *4
+'hakanhakanhakanhakan'
+>>> my_hobby = "fgg"
+>>> "g" in my_hobby
+True
+>>> s = "123456789"
+>>> s[3}
+SyntaxError: invalid syntax
+>>> s = "ajdjdjkdf"
+>>> s[4]
+'d'
+>>> #Slicing...
+>>> s[3: ]
+'jdjkdf'
+>>> s[1:6:3]
+'jd'
+>>> s[::-1]
+'fdkjdjdja'
+>>> 
